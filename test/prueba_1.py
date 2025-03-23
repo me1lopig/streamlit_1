dc993ab9c8f2092e9d763fa1fa9dd5840144d95e
me1lopig <me1lopig@uco.es>
2025-03-23 17:42:00 +0100

    20250323

---
+++ test/prueba_1.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title("Cálculo de valores y direcciones principales")
+    st.write("Introduce los valores del tensor ")
+
+
+
+if __name__ == "__main__":
+    main()
