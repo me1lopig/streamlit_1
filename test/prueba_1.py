228e8b4d2689bbbce5f50d1d429146e334be2150
me1lopig <me1lopig@uco.es>
2025-03-25 22:09:56 +0100

    20250325

--- test/prueba_1.py
+++ test/prueba_1.py
@@ -4,6 +4,10 @@ def main():
     st.title("Cálculo de valores y direcciones principales")
     st.write("Introduce los valores del tensor ")
 
+    
+    value = st.number_input(f'Elemento ({1}, {1})', key=f'{1}{1}')
+    st.write("Valor ",value)
+
 
 
 if __name__ == "__main__":
